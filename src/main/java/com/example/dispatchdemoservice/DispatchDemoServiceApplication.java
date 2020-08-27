c567019d963f98bda4a5f467e680f57163f223da
Eric Goode <egoode@mesosphere.com>
2020-08-27 13:32:49 -0500

    Initial commit

---
+++ src/main/java/com/example/dispatchdemoservice/DispatchDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dispatchdemoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchDemoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dispatchdemoservice/DispatchDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dispatchdemoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
